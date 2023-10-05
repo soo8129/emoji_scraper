7863a522f4f13c9bffd0225f688bcb8951aa581f
Kim Soo Hyeok <soohyeok_kim@tmax.co.kr>
2023-10-05 10:52:06 +0900

    minor fix

--- db_insert.py
+++ db_insert.py
@@ -1,5 +1,7 @@
+import re
+
 def emoji_insert(cursor, data):
     sql = """INSERT INTO CONTENT_COMMENT VALUES (DEFAULT, ?, ?)"""
-    cursor.executemany(sql, [(col['label'], col['hexcode']) for col in data])
+    cursor.executemany(sql, [(col['label'], col['hexcode']) for col in data if (not re.match(r"^[^가-힣]*$", col['label']) or col['label']=='DNA')])
     cursor.commit()
     return
